896ce220fb1be445f6e35d706cadc56a25324ecd
Nele Liis Sams <nesams@ttu.ee>
2022-02-14 09:34:55 +0200

    ringclass

--- PR/PR03Ring/src/ee/taltech/iti0202/lotr/Ring.java
+++ PR/PR03Ring/src/ee/taltech/iti0202/lotr/Ring.java
@@ -1,4 +1,26 @@
 package ee.taltech.iti0202.lotr;
 
+
 public class Ring {
+    private final Type ringType;
+    private final Material ringMaterial;
+
+
+    enum Type {
+        THE_ONE, GOLDEN, NENYA, OTHER
+    }
+    enum Material {
+        GOLD, SILVER, BRONZE, PLASTIC, DIAMOND
+    }
+    public Ring(Type type, Material material) {
+        this.ringType = type;
+        this.ringMaterial = material;
+    }
+    public Type getType(){
+        return this.ringType;
+    }
+    public Material getMaterial(){
+        return this.ringMaterial;
+    }
+
 }
