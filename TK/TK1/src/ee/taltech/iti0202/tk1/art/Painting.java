cd9ff3d7b075066f0ed063392398bbb42260cf42
Nele Liis Sams <nesams@ttu.ee>
2022-02-23 14:59:23 +0200

    tk1

---
+++ TK/TK1/src/ee/taltech/iti0202/tk1/art/Painting.java
@@ -0,0 +1,53 @@
+package ee.taltech.iti0202.tk1.art;
+
+public class Painting {
+    private String title;
+    private String author;
+    /**
+     * Constructor.
+     */
+    public Painting(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    /**
+     * Constructor.
+     */
+    public Painting(String title) {
+        this.title = title;
+        this.author = null;
+    }
+    /**
+     * Setter.
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    /**
+     * Setter.
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    /**
+     * Getter.
+     */
+    public String getTitle() {
+        return title;
+    }
+    /**
+     * Getter.
+     */
+    public String getAuthor() {
+        return author;
+    }
+    public String toString(Painting p) {
+        if (p.author != null) {
+            String result = "This is a painting named " + p.title + " and made by" + p.author + ".";
+            return result;
+        } else {
+            String result = "This is a painting named " + p.title + " and made by an unknown artist.";
+            return result;
+        }
+    }
+}
